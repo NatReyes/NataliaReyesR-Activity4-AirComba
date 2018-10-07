c7cc7fd816155d3e55312a7d5df25f29732f1269
NatReyes <41844595+NatReyes@users.noreply.github.com>
2018-10-06 19:03:45 -0500

    Add new Empty

---
+++ Natalia Reyes-AirCombat/Assets/Scripts/FighterControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Right
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(Vector3.right);
+        }
+
+        // Left
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(Vector3.left);
+        }
+
+        // Up
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(Vector3.up);
+        }
+
+        // Down
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(Vector3.down);
+        }
+
+        //Space shooting
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject newMissile = GameObject.Instantiate(Resources.Load("Bullets/Missile") as GameObject);
+            GameObject shootPivot = GameObject.Find("Fighter/ShootPivot");
+            newMissile.transform.SetParent(shootPivot.transform);
+            newMissile.transform.localPosition = Vector3.zero;
+            newMissile.transform.SetParent(null);
+
+        }
+    
+    }
+}
---
+++ Natalia Reyes-AirCombat/Assets/Scripts/MisilMotion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MisilMotion : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(Vector3.forward * speed);
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Colision dectectada");
+        GameObject enemy = collision.collider.gameObject;
+        GameObject.Destroy(enemy);
+        GameObject missile = this.gameObject;
+        GameObject.Destroy(missile);
+    }
+    
+}
